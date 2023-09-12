245bcf03cdc211ea811d9cfba1bd25bc28c6cc41
Andrew <andrewahti@gmail.com>
2023-09-12 22:52:52 +0300

    Переделал аннотации контроллера

--- src/main/java/com/testTask/TestTask/Controllers/SymbolCounterController.java
+++ src/main/java/com/testTask/TestTask/Controllers/SymbolCounterController.java
@@ -4,18 +4,21 @@ import com.testTask.TestTask.Services.SymbolCounterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+@Controller
+@RequestMapping("/counter")
 @RequiredArgsConstructor
 public class SymbolCounterController {
     private final SymbolCounterService symbolCounterService;
 
-    @GetMapping("/counter/chars")
+    @GetMapping("/chars")
     public ResponseEntity<Map<Character, Integer>> getCharactersCountInMessage(@RequestBody String row) {
         Map<Character, Integer> result = symbolCounterService.getCharactersCountFromStringRow(row);
         if (result == null) {
